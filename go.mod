5a70880b457ffbfa7cc96ed90acbd59bf83efc7d
shaocong <shaocong_mo@163.com>
2020-10-10 16:00:43 +0800

    支持端口、修改上传文件机制

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/melbahja/goph
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pkg/sftp v1.11.0
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
--- sftp.go
+++ sftp.go
@@ -4,6 +4,7 @@
 package goph
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"github.com/pkg/sftp"
@@ -12,32 +13,38 @@ import (
 
 // Upload local file to remote.
 func Upload(c *ssh.Client, src string, dest string) (err error) {
-
-	client, err := sftp.NewClient(c)
-
-	if err != nil {
-		return
+	if session, err := c.NewSession(); err == nil {
+		defer session.Close()
+		go func() {
+			fmt.Println("start upload file:", src)
+			Buf := make([]byte, 1024)
+			w, _ := session.StdinPipe()
+			defer w.Close()
+			File, _ := os.Open(src)
+			info, _ := File.Stat()
+			fmt.Fprintln(w, "C0644", info.Size(), info.Name())
+			for {
+				n, err := File.Read(Buf)
+				fmt.Fprint(w, string(Buf[:n]))
+				if err != nil {
+					if err == io.EOF {
+						return
+					} else {
+						fmt.Println("Upload File error:", err)
+						return
+					}
+				}
+			}
+		}()
+		if err := session.Run(fmt.Sprintf("/usr/bin/scp -qrt %s", dest)); err != nil {
+			if err != nil {
+				if err.Error() != "Process exited with: 1. Reason was: ()" {
+					fmt.Println(err.Error())
+				}
+			}
+			fmt.Printf("upload file %s finish\n", src)
+		}
 	}
-
-	defer client.Close()
-
-	srcFile, err := os.Open(src)
-
-	if err != nil {
-		return
-	}
-
-	defer srcFile.Close()
-
-	destFile, err := client.Create(dest)
-
-	if err != nil {
-		return
-	}
-
-	defer destFile.Close()
-
-	_, err = io.Copy(destFile, srcFile)
 	return
 }
 
